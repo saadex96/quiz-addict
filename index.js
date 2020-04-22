4e6edcdf2b8350c8a0c6beb95d1895411847b29e
damien <damien@macbook-pro-de-damien.home>
2020-04-22 16:16:22 +0200

    add express / ejs

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'));
+
+app.set('views', './views')
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('index')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
